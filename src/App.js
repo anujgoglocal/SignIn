c2f10cc51cafcb7733d6f2691a17e01afea177a1
anujgoglocal <anuj.k@goglocal.live>
2022-05-14 01:44:37 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from 'react'
+import SignInForm from "./Components/SignInForm.js"
+import SignUpForm from "./Components/SignUpForm.js"
 import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Link,
+    Switch,
+    Route,
+    withRouter
+  } from "react-router-dom";
+import Otp from './Components/Otp.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      {/* <div className="App">  */}
+          {/* <div className="appAside" />
+          <div className="appForm"> */}
+                <Router>
+                 <Switch>
+                   <Route  exact path="/" >
+                          <SignInForm />
+                   </Route>
+                    <Route path='/SignUpForm'  >
+                        <SignUpForm />
+                   </Route>
+                   <Route path='/Otp'>
+                        <Otp />
+                   </Route>
+                  </Switch>
+                </Router>
+            {/* </div> */}
+        {/* </div> */}
+       
+
+     </>
   );
 }
 
